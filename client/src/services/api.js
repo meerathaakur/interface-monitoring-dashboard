60a44cf9a9c0fcea9035400db0b4799b8cfb6181
meerathaakur <jayants2859@gmail.com>
2025-08-05 00:32:51 +0530

    final touch up

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api=axios.create({
-    baseURL:"http://localhost:8080/api"
+    baseURL:"https://interface-monitoring-dashboard.onrender.com/api"
 })
 
 export default api
